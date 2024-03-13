a9e6503c75af3635b259758551bbc63c80248b01
Juannnrv <160557063+Juannnrv@users.noreply.github.com>
2024-03-13 08:36:26 -0500

    Crear petición y agregar opción de guardar a la menú

--- modules/getProducto.py
+++ modules/getProducto.py
@@ -1,9 +1,17 @@
-import storage.producto as pr
+import requests
 from tabulate import tabulate
+import modules.postProducto as psProducto
+
+def getAllData():
+   #json-server storage/producto.json -b 5502
+   peticion = requests.get("http://172.16.100.116:5502")
+   data = peticion.json()
+
+   return data
 
 def getAllStocksPriceGama():
     stockPriceGama = []
-    for val in pr.producto:
+    for val in getAllData():
         gama = val.get('gama')
         stock = val.get('cantidad_en_stock')
         descripcion = val.get('descripcion')
@@ -18,15 +26,13 @@ def getAllStocksPriceGama():
     stockPriceGama.sort(key=lambda x: x['precio_venta'], reverse=True)
     return stockPriceGama
 
-
-
 def menu():
     while True:
      print("""
         ---REPORTES DE LOS PRODUCTOS---
           
           1. Obtener y ordenar los precios de los productos de la gama ornamental de mayor a menor 
-          2. Obtener  
+          2. Guardar 
          
        Presiona (Ctrl + C) para regresar al menú principal
 """)
@@ -35,6 +41,20 @@ def menu():
       opcion = int(input('Seleccione una de las opciones => '))
       if opcion == 1:
        print(tabulate(getAllStocksPriceGama(), headers = "keys", tablefmt= "fancy_grid"))
+      elif opcion == 2:
+         producto = {
+             'codigo_producto': input('Ingrese el codigo del producto => '),
+             'nombre': input('Ingrese el nombre del producto => '),
+             'gama': input('Ingrese la gama del producto => '),
+             'dimensiones': input('Ingrese las dimensiones del producto => '),
+             'proveedor': input('Ingrese el proveedor del producto => '),
+             'descripcion': input('Ingrese la descripcion del producto => '),
+             'cantidad_en_stock': int(input('Ingrese la cantidad en stock del producto => ')),
+             'precio_venta': int(input('Ingrese el precio de venta del producto => ')),
+             'precio_proveedor': int(input('Ingrese el precio del producto'))
+         }
+         psProducto.postProducto(producto)
+         print("Producto Guardado")
      except KeyboardInterrupt:
          print()
          print()
