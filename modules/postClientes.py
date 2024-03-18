61442bb7c600230d0e8e9971fbcef0195d4d9627
Juannnrv <160557063+Juannnrv@users.noreply.github.com>
2024-03-17 21:22:38 -0500

    Validaciones completadas

--- modules/postClientes.py
+++ modules/postClientes.py
@@ -28,70 +28,68 @@ def postClientes():
 
            if(not cliente.get('nombre_cliente')):
                 nombre = input('Ingrese el nombre del cliente => ')
-                if(re.match(r'^[A-Z]{2}-[0-9]{3}$', nombre) is not None):
+                if(re.match(r'^[A-Za-z\s]+$', nombre)):
                     cliente['nombre_cliente'] = nombre
 
            if(not cliente.get('nombre_contacto')):
                 nombre = input('Ingrese el nombre de contacto del cliente => ')
-                if(re.match(r'^[A-Z]{2}-[0-9]{3}$', nombre) is not None):
+                if(re.match(r'^[A-Za-z\s]+$', nombre)):
                     cliente['nombre_contacto'] = nombre  
 
            if(not cliente.get('apellido_contacto')):
                 apellido = input('Ingrese el apellido de contacto del cliente => ')
-                if(re.match(r'^[A-Z]{2}-[0-9]{3}$', apellido) is not None):
+                if(re.match(r'^[A-Za-z]+$', apellido)):
                     cliente['apellido_contacto'] = apellido
                     
            if(not cliente.get('telefono')):
                 telefono = input('Ingrese el telefono del cliente => ')
-                if(re.match(r'^[A-Z]{2}-[0-9]{3}$', telefono) is not None):
+                if(re.match(r'^\d+(\s*\d+)*$', telefono)):
                     cliente['telefono'] = telefono
         
            if(not cliente.get('fax')):
                 fax = input('Ingrese el fax del cliente => ')
-                if(re.match(r'^[A-Z]{2}-[0-9]{3}$', fax) is not None):
+                if(re.match(r'^\d+(\s*\d+)*$', fax)):
                     cliente['fax'] = fax     
 
            if(not cliente.get('linea_direccion1')):
-                direccion_1 = input('Ingrese la diección 1 del cliente => ')
-                if(re.match(r'^[A-Z]{2}-[0-9]{3}$', direccion_1) is not None):
+                direccion_1 = input('Ingrese la dirección 1 del cliente => ')
+                if(re.match(r'^[^\n]+$', direccion_1)):
                     cliente['direccion_1'] = direccion_1    
 
            if(not cliente.get('linea_direccion2')):
-                direccion_2 = input('Ingrese la diección 1 del cliente => ')
-                if(re.match(r'^[A-Z]{2}-[0-9]{3}$', direccion_2) is not None):
+                direccion_2 = input('Ingrese la dirección 2 del cliente => ')
+                if(re.match(r'^[^\n]+$', direccion_2)):
                     cliente['direccion_2'] = direccion_2
 
            if(not cliente.get('ciudad')):
-                ciudad = input('Ingrese la diección 1 del cliente => ')
-                if(re.match(r'^[A-Z]{2}-[0-9]{3}$', ciudad) is not None):
+                ciudad = input('Ingrese la ciudad del cliente => ')
+                if(re.match(r'^[A-Za-z\s]+$', ciudad)):
                     cliente['ciudad'] = ciudad
 
            if(not cliente.get('region')):
                 region = input('Ingrese la región del cliente => ')
-                if(re.match(r'^[A-Z]{2}-[0-9]{3}$', region) is not None):
+                if(re.match(r'^[A-Za-z\s]+$', region)):
                     cliente['region'] = region    
 
            if(not cliente.get('pais')):
                 pais = input('Ingrese el país del cliente => ')
-                if(re.match(r'^[A-Z]{2}-[0-9]{3}$', pais) is not None):
+                if(re.match(r'^[A-Za-z\s]+$', pais)):
                     cliente['pais'] = pais
 
            if(not cliente.get('codigo_postal')):
                 codigo_postal = input('Ingrese el código postal del cliente => ')
-                if(re.match(r'^[A-Z]{2}-[0-9]{3}$', codigo_postal) is not None):
+                if(re.match(r'^[0-9]+$', codigo_postal)):
                     cliente['codigo_postal'] = codigo_postal 
 
            if(not cliente.get('codigo_empleado_rep_ventas')):
                 codigo_empleado_rep_ventas = input('Ingrese el código del representante de ventas asignado al cliente => ')
-                if(re.match(r'^[A-Z]{2}-[0-9]{3}$', codigo_empleado_rep_ventas) is not None):
-                    codigo_empleado_rep_ventas = int(codigo_empleado_rep_ventas)
+                if codigo_empleado_rep_ventas.isdigit():
                     cliente['codigo_empleado_rep_ventas'] = codigo_empleado_rep_ventas
 
            if(not cliente.get('limite_credito')):
                 limite_credito = input('Ingrese el límite crediticio del cliente => ')
-                if(re.match(r'^[A-Z]{2}-[0-9]{3}$', limite_credito) is not None):
-                    limite_credito = int(limite_credito)
-                    cliente['limite_credito'] = limite_credito  
+                if re.match(r'^[0-9]+(\.[0-9]+)?$', limite_credito):
+                    cliente['limite_credito'] = float(limite_credito)
                     break                                 
            
            else:
