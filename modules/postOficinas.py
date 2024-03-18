5fa6bbf08c50e8eb63613786da2cd708f8bb6165
Juannnrv <160557063+Juannnrv@users.noreply.github.com>
2024-03-17 19:12:20 -0500

    Manejo de errores y validaciones para las nuevas oficinas

--- modules/postOficinas.py
+++ modules/postOficinas.py
@@ -27,33 +27,48 @@ def postOficina():
 
             if not oficina.get('ciudad'):
                 ciudad = input('Ingrese la ciudad de la oficina => ')
-                if ciudad[0].isupper() and ciudad[1:].isalpha(): 
+                if re.match(r'^[A-Za-z\s]$', ciudad): 
                     oficina['ciudad'] = ciudad
 
             if not oficina.get('pais'):
                 pais = input('Ingrese el país de la oficina => ')
-                if ciudad[0].isupper() and ciudad[1:].isalpha():
+                if re.match(r'^[A-Za-z]$', pais):
                     oficina['pais'] = pais
 
             if not oficina.get('region'):
-                region = input('Ingrese la region de la oficina => ')
-                if ciudad[0].isupper() and ciudad[1:].isalpha():
+                region = input('Ingrese la región de la oficina => ')
+                if re.match(r'^[A-Za-z\s-]+$', region):
                     oficina['region'] = region
 
             if not oficina.get('codigo_postal'):
                 codigo_postal = input('Ingrese el código postal de la oficina => ')
-                if re.match(r'^[0-9]{5}|[A-Z0-9]{4}\s[A-Z0-9]{3}|[A-Z]{3}\s[0-9]{4}|[0-9]{3}-[0-9]{4}$', codigo_postal):
+                if re.match(r'^[a-zA-Z\w\s-]+$', codigo_postal):
                     oficina['codigo_postal'] = codigo_postal
+
+            if not oficina.get('telefono'):
+                telefono = input('Ingrese el teléfono de la oficina => ')
+                if re.match(r'^\+\d+(\s\d+)*$', telefono):
+                    oficina['telefono'] = telefono
+
+            if not oficina.get('linea_direccion1'):
+                linea_direccion1 = input('Ingrese la dirección 1 de la oficina => ')
+                if re.match(r'^[^\n]+$', linea_direccion1):
+                    oficina['linea_direccion1'] = linea_direccion1
+
+            if not oficina.get('linea_direccion2'):
+                linea_direccion2 = input('Ingrese la dirección 2 de la oficina => ')
+                if re.match(r'^[^\n]+$', linea_direccion2):
+                    oficina['linea_direccion2'] = linea_direccion2
 
         except Exception as error:
             print('---ERROR---')
             print(error)
             
-    headers = {'Content-Type': 'application/json', 'charser': 'UTF-8'}
-    peticion = requests.post('http://192.168.1.7:5502', headers=headers, data=json.dumps(oficina))
-    res = peticion.json()
-    res ['Mensaje'] = 'Oficina Guardada'
-    return [res]
+        headers = {'Content-Type': 'application/json', 'charser': 'UTF-8'}
+        peticion = requests.post('http://192.168.1.7:5502', headers=headers, data=json.dumps(oficina))
+        res = peticion.json()
+        res ['Mensaje'] = 'Oficina Guardada'
+        return [res]
 
 
 def menu():
